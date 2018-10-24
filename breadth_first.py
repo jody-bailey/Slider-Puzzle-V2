6d222f82746e17c32c33795fcbab61fa479ddfcb
Jody Bailey <jodybailey93@gmail.com>
2018-10-24 02:54:56 -0500

    got it running a few loops but moving random numbers

--- breadth_first.py
+++ breadth_first.py
@@ -7,16 +7,41 @@ class BreadthSearch:
     """Needs comments"""
     counter = 0
     state_array = []
-    visited = {}
+    path = {}
     node = {}
     queue = deque([])
+    visited = []
 
     def __init__(self, node):
         self.counter = 0
         self.state_array = node['state_array']
-        self.visited = {'{}'.format(self.counter): node['state_string']}
+        self.path = {'{}'.format(self.counter): node['state_string']}
         self.node = node
+        self.visited = [node['state_string']]
         self.queue = deque([node])
+
+    def create_note(self, state_array, state_string, path):
+        """Needs comments"""
+        node = {'state_array': state_array,
+                'state_string': state_string,
+                'depth': 0,
+                'heuristic': 0,
+                'path': path}
+        return node
+
+    def create_array(self, state_string):
+        """Needs comments"""
+        array = [0] * 3
+        for i in range(3):
+            array[i] = [0] * 3
+
+        index = 0
+        for i in range(3):
+            for j in range(3):
+                array[i][j] = int(state_string[index])
+                index += 1
+
+        return array
 
     def complete(self, node):
         """Needs comments"""
@@ -42,15 +67,15 @@ class BreadthSearch:
 
     def add_to_visited(self, node):
         """Needs comments"""
-        self.visited.update({'{}'.format(self.counter): node['state_string']})
+        self.path.update({'{}'.format(self.counter): node['state_string']})
 
     def check_visited(self):
         """Needs comments"""
-        self.visited.fromkeys(print(value) for value in self.visited)
+        self.path.fromkeys(print(value) for value in self.path)
 
-    def locate_hole(self):
+    def locate_hole(self, node):
         """Needs comments"""
-        array = self.node['state_array']
+        array = node['state_array']
         for i in range(3):
             for j in range(3):
                 if array[i][j] == 0:
@@ -70,35 +95,53 @@ class BreadthSearch:
             return True
 
     def swap_locations(self, node, location, new_loc):
+        """Needs comments"""
         test_node = node
         test_node[location[0]][location[1]], test_node[new_loc[0]][new_loc[1]] = \
             test_node[new_loc[0]][new_loc[1]], test_node[location[0]][location[1]]
         return test_node
 
+    def add_moves_to_queue(self, moves):
+        """Needs comments"""
+        for move in moves:
+            if move not in self.path.values():
+                array = self.create_array(move)
+                node = self.create_note(array, move, self.path)
+                self.queue.append(node)
+                if move not in self.visited:
+                    self.visited.append(move)
+
+    def make_move(self, node):
+        """Needs comments"""
+        self.count_up()
+        self.node = deepcopy(node)
+        self.path.update({'{}'.format(self.counter): self.get_state_string(node['state_array'])})
+
     def check_moves(self, location):
+        """Needs comments"""
         possible_moves = []
-        # check left
+        # check up
         new_loc = location[0] - 1, location[1]
         if self.check_bounds(new_loc):
             test_node = deepcopy(self.state_array)
             test_node = self.swap_locations(test_node, location, new_loc)
             possible_moves.append(self.get_state_string(test_node))
 
-        # check right
+        # check down
         new_loc = location[0] + 1, location[1]
         if self.check_bounds(new_loc):
             test_node = deepcopy(self.state_array)
             test_node = self.swap_locations(test_node, location, new_loc)
             possible_moves.append(self.get_state_string(test_node))
 
-        # check up
+        # check left
         new_loc = location[0], location[1] - 1
         if self.check_bounds(new_loc):
             test_node = deepcopy(self.state_array)
             test_node = self.swap_locations(test_node, location, new_loc)
             possible_moves.append(self.get_state_string(test_node))
 
-        # check down
+        # check right
         new_loc = location[0], location[1] + 1
         if self.check_bounds(new_loc):
             test_node = deepcopy(self.state_array)
@@ -107,11 +150,28 @@ class BreadthSearch:
 
         return possible_moves
 
+    def print_array(self, array):
+        for row in array:
+            print(' '.join(str(elem) for elem in row))
+
     def run(self):
         """Needs comments"""
         while len(self.queue) > 0:
             this_node = self.queue.popleft()
             if not self.complete(this_node['state_array']):
-                location = self.locate_hole()
+                location = self.locate_hole(this_node)
                 moves = self.check_moves(location)
+                self.add_moves_to_queue(moves)
+                try:
+                    self.make_move(self.queue[0])
+                    temp = self.queue[0]
+                    array = temp['state_array']
+                    self.print_array(array)
+                except IndexError:
+                    continue
                 print(moves)
+            else:
+                return
+
+        my_array = self.node['state_array']
+        print(''.join(str(elem) for row in my_array for elem in row))
